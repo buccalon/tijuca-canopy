99feb452762ec277bcc013cc3889c2deb56a2606
Bruno Buccalon <buccalon@brunos-mbp.dyndns.rice.edu>
2024-02-06 18:02:44 -0600

    add @martimpassos thumbnail fix

--- src/lib/iiif/image.js
+++ src/lib/iiif/image.js
@@ -1,14 +1,42 @@
 const { createThumbnailHelper } = require("@iiif/vault-helpers");
 
-exports.getRepresentativeImage = async (canvas, preferredSize = 1200) => {
-  const helper = createThumbnailHelper();
+const getService = async (service, preferredWidth = 1200) => {
   try {
-    return await helper.getBestThumbnailAtSize(canvas, {
-      width: preferredSize,
-      height: preferredSize,
+    const response = await axios.get(`${service["id"]}/info.json`);
+    const { width, height } = response.data.sizes.reduce((a, b) => {
+      return Math.abs(b.width - preferredWidth) <
+        Math.abs(a.width - preferredWidth)
+        ? b
+        : a;
     });
-  } catch (error) {
-    console.log(error);
+    return {
+      id: `${service["id"]}/full/${width},${height}/0/default.jpg`,
+      width: width,
+      height: height,
+    };
+  } catch (err) {
+    console.log(err.response.status);
     return {};
   }
 };
+
+exports.getRepresentativeImage = async (resource, preferredSize = 1200) => {
+  const firstCanvas = resource.items[0].items[0].items[0];
+  const firstCanvasService = await getService(
+    firstCanvas.body.service[0],
+    preferredSize
+  );
+  if (firstCanvasService) {
+    return {best:
+      {
+        id: firstCanvasService.id,
+        type: "Image",
+        format: "image/jpeg",
+        width: firstCanvasService.width,
+        height: firstCanvasService.height,
+      },
+    };
+  } else {
+    return resource.thumbnail ? resource.thumbnail : [];
+  }
+}
