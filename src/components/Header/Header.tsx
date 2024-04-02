7dbd438deeba04bfc448be51d2f27406fd178e49
bruno <buccalon@users.noreply.github.com>
2024-04-01 19:31:27 -0500

    remove unsed vars

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,9 @@
 import { Actions, ResponsiveActions } from "./Header.styled";
-import { Content, Title, Wrapper } from "@components/Header/Header.styled";
+import { Content, Wrapper } from "@components/Header/Header.styled";
 import React, { useEffect, useState } from "react";
 
 
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
-import { Label } from "@samvera/clover-iiif/primitives";
 import Link from "next/link";
 import Locale from "@components/Shared/Locale/Locale";
 import Nav from "@components/Nav/Nav";
